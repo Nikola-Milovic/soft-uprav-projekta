2fde5c30178c02c27733ef16e75a02a4c319907e
Nikola Milovic <nikolamilovic2001@gmail.com>
2023-10-21 14:23:25 +0200

    sredio izgled pocetne i futera

--- app/pogledi/delovi/futer.php
+++ app/pogledi/delovi/futer.php
@@ -1,7 +1,65 @@
-    <footer class="bg-light text-center text-lg-start">
-        <div class="container p-4">
-            <p class="text-muted">© 2023 Eco Friendly Food Delivery</p>
+<footer class="bg-light text-center text-lg-start pt-5">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <h5>Kontakt Informacije</h5>
+                <p>
+                    Ulica Maršala Tita 58, <br>
+                    11000 Beograd, Srbija <br>
+                    Tel: +381 11 2345 678 <br>
+                    Email: info@ekofood.com
+                </p>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <h5>Pratite nas</h5>
+                <a href="#" class="btn btn-outline-primary btn-sm mb-2">
+                    <i class="fab fa-facebook-f"></i> Facebook
+                </a>
+                <a href="#" class="btn btn-outline-primary btn-sm mb-2">
+                    <i class="fab fa-instagram"></i> Instagram
+                </a>
+                <a href="#" class="btn btn-outline-primary btn-sm mb-2">
+                    <i class="fab fa-twitter"></i> Twitter
+                </a>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <h5>Brze Poveznice</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/">Početna</a></li>
+                    <li><a href="/meni">Meni</a></li>
+                    <li><a href="/onama">O nama</a></li>
+                    <li><a href="/prijava">Prijava</a></li>
+                </ul>
+            </div>
         </div>
-    </footer>
+				<button id="scrollTopBtn" class="">
+						<i class="fas fa-arrow-up text-white"></i> Top
+				</button>
+
+			<div class="text-muted p-3 text-center">
+				© 2023 Eko Hrana
+			</div>
+    </div>
+</footer>
+
+<script>
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 1200) {
+                $('#scrollTopBtn').fadeIn();
+            } else {
+                $('#scrollTopBtn').fadeOut();
+            }
+        });
+
+        $('#scrollTopBtn').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+    });
+</script>
+
 </body>
 </html>
--- app/pogledi/pocetna.pogled.php
+++ app/pogledi/pocetna.pogled.php
@@ -2,19 +2,87 @@
 include 'delovi/heder.php';  
 ?>
 
-<div class="container my-5 animated fadeIn">
-    <div class="row align-items-center">
-        <div class="col-md-6">
-            <h1 class="display-4">Dostava Eko Hrane</h1> 
-            <p class="lead">Dostavljamo vašu omiljenu hranu u ekološki prihvatljivim pakovanjima električnim vozilima.</p> 
-            <hr class="my-4">
-            <p>Pridružite se u činjenju planete zelenijom, svaka dostava znači.</p> 
-            <a class="btn btn-primary btn-lg animated fadeInUp" href="#" role="button">Naruči Odmah</a> 
+<main class="container">
+<div class="container-fluid p-0 position-relative">
+    <div class="row align-items-center hero-section">
+        <div class="col-lg-6 py-5 px-lg-5 text-center text-lg-start">
+            <h1 class="display-4">Dostava Eko Hrane</h1>
+            <p class="lead">Dostavljamo vašu omiljenu hranu u ekološki prihvatljivim pakovanjima električnim vozilima.</p>
+            <a class="btn btn-primary btn-lg" href="/meni" role="button">Naruči Odmah</a> 
         </div>
-        <div class="col-md-6">
-            <img src="assets/slike/hero.jpg" alt="Eko Dostava Hrane" class="img-fluid animated fadeInRight"> 
+        <div class="col-lg-6 p-0">
+            <img src="assets/slike/hero.jpg" alt="Eko Dostava Hrane" class="img-fluid">
         </div>
     </div>
 </div>
 
+<div class="container my-5">
+    <div class="row align-items-center">
+        <div class="col-md-5 order-md-2">
+            <img src="assets/slike/eco-friendly.jpg" alt="Ekološki prihvatljivo" class="img-fluid rounded">
+        </div>
+        <div class="col-md-7">
+            <h2>Ekološki Orijentisani</h2>
+            <p>Naša dostava hrane se izvršava koristeći električna vozila, smanjujući emisiju ugljen-dioksida i doprinoseći čistijem vazduhu. Koristimo biodegradabilna i reciklirana pakovanja koja su bezbedna za okolinu, smanjujući plastični otpad.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container my-5 bg-light p-5 rounded">
+    <div class="row align-items-center">
+		<div class="col-lg-4">
+			<img src="assets/slike/healthy-food.webp" alt="Zdrava Hrana" class="img-fluid rounded">
+		</div>
+		<div class="col-lg-8">
+				<h2>Zdrava i Kvalitetna Hrana</h2>
+				<p>Svi naši sastojci su organski, bez upotrebe pesticida i hemikalija, obezbeđujući vam najkvalitetnije obroke. Naša ambalaža je dizajnirana da eliminiše mikroplastiku, čuvajući vašu hranu čistom i zdravom.</p>
+		</div>
+    </div>
+</div>
+
+<div class="container my-5 text-center">
+    <h2 class="mb-2">Iskustva Naših Klijenata</h2>
+    <hr class="divider mb-3">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>"Najbolja dostava hrane! Sve je ekološki i zdravo, obožavam!"</p>
+                        <footer class="blockquote-footer">Ana</footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>"Brza dostava i odlična hrana. Plus, osećam se dobro jer znam da je sve ekološki."</p>
+                        <footer class="blockquote-footer">Marko</footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>"Fantastična usluga! Hrana je uvek sveža, a dostava brza i ekološka."</p>
+                        <footer class="blockquote-footer">Jovana</footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="cta-bg rounded p-5 text-white text-center">
+        <h2 class="mb-4">Pravi Izbor za Vas i Planetu</h2>
+        <p class="lead">Uživajte u ukusnoj, zdravoj i ekološki odgovornoj hrani. Neka svaka vaša porudžbina doprinese zelenijoj planeti.</p>
+        <a href="/meni" class="btn btn-light btn-lg">Naruči Sada</a>
+    </div>
+</div>
+</main>
 <?php include 'delovi/futer.php'; ?>  
